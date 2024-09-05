a652a364b52c35cc4978d52c8f88b3fdf926bbf4
SimoneDUrso <simone.durso98@gmail.com>
2024-09-05 14:15:00 +0200

    [UPD] Added foreach to visualize on screen the hotel's name with var_dump

--- index.php
+++ index.php
@@ -36,6 +36,10 @@
             'distance_to_center' => 50
         ],
     ];
+
+    foreach($hotels as $hotel){
+        var_dump($hotel['name']);
+    }
 ?>
 
 <!DOCTYPE html>
@@ -43,9 +47,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Hotels</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                
+            </div>
+        </div>
+    </div>
 </body>
 </html>
